41a516880d3b57c40e703c629d58e75b8738f702
Yeasir Ahamed <yahamed007@gmail.com>
2021-10-14 17:06:39 +0600

    add Either Left

--- utils/utils.js
+++ utils/utils.js
@@ -27,3 +27,9 @@ export const Right = x => ({
   map: f => Right(f(x)),
   fold: (f, g) => g(x),
 })
+
+export const Left = x => ({
+  chain: f => Left(x),
+  map: f => Left(x),
+  fold: (f, g) => f(x),
+})

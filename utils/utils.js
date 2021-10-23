c9aedd269ca4987d5e2a6c275fce79eca0f8b06e
Yeasir Ahamed <yahamed007@gmail.com>
2021-10-23 22:54:46 +0600

    add Task

--- utils/utils.js
+++ utils/utils.js
@@ -42,4 +42,38 @@ export const tryCatch = f => {
   }
 }
 
-export const fromNullable = x => (x != null ? Right(x) : Left(x));
+export const fromNullable = x => (x != null ? Right(x) : Left(x))
+
+export const Task = fork => ({
+  fork,
+  ap: other =>
+    Task((rej, res) => fork(rej, f => other.fork(rej, x => res(f(x))))),
+  map: f => Task((rej, res) => fork(rej, x => res(f(x)))),
+  chain: f => Task((rej, res) => fork(rej, x => f(x).fork(rej, res))),
+  concat: other =>
+    Task((rej, res) =>
+      fork(rej, x =>
+        other.fork(rej, y => {
+          console.log('X', x, 'Y', y)
+          res(x.concat(y))
+        }),
+      ),
+    ),
+  fold: (f, g) =>
+    Task((rej, res) =>
+      fork(
+        x => f(x).fork(rej, res),
+        x => g(x).fork(rej, res),
+      ),
+    ),
+})
+Task.of = x => Task((rej, res) => res(x))
+Task.rejected = x => Task((rej, res) => rej(x))
+Task.fromPromised =
+  fn =>
+  (...args) =>
+    Task((rej, res) =>
+      fn(...args)
+        .then(res)
+        .catch(rej),
+    )

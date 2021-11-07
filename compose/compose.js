de5c3ef9ad7838ca516616ce4aaff55c4284828f
Yeasir Ahamed <yahamed007@gmail.com>
2021-11-07 13:33:59 +0600

    update comment

--- compose/compose.js
+++ compose/compose.js
@@ -16,17 +16,3 @@ const capitalizeString = compose(
 capitalizeString('capitalize the first letter of each word in a string')
 
 // Capitalize The First Letter Of Each Word In A String
-
-// It also work same
-/*
-const capitalizeString = compose(
-  compose(arrToString, uppercaseFirstCharOfArrStr),
-  stringToArr
-);
-
-// or
-
-const returnedFn = compose(uppercaseFirstCharOfArrStr, stringToArr);
-const capitalizeString = compose(arrToString, returnedFn);
-
-*/

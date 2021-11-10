6f73bb0e7376494ec1d5f588c6b66aaa706ae881
Yeasir Ahamed <yahamed007@gmail.com>
2021-11-10 16:39:46 +0600

    update identity functor example

--- identityFunctor/identityFunctor.js
+++ identityFunctor/identityFunctor.js
@@ -1,10 +1,16 @@
-import {identityFunctor} from '../utils/utils.js'
+import {Id, compose} from '../utils/utils.js'
 
-const capitalizeString = identityFunctor(
+const stringToArr = str => str.split(' ')
+
+const uppercaseFirstCharOfArrStr = arr =>
+  arr.map(str => str.charAt(0).toUpperCase() + str.slice(1))
+
+const arrToString = arr => arr.join(' ')
+
+const capitalizeString = Id.of(
   'capitalize the first letter of each word in a string',
 )
-  .map(str => str.split(' '))
-  .map(arr => arr.map(str => str.charAt(0).toUpperCase() + str.slice(1)))
-  .fold(arr => arr.join(' '))
+  .map(compose(arrToString, uppercaseFirstCharOfArrStr, stringToArr))
+  .extract()
 
 // Capitalize The First Letter Of Each Word In A String

26574f1da58b1eca202f7c8ffea7888793d6cba9
Yeasir Ahamed <yahamed007@gmail.com>
2021-11-12 05:01:13 +0600

    refactor Either

--- utils/utils.js
+++ utils/utils.js
@@ -60,27 +60,48 @@ Id.of = x => Id(x)
 
 export const Log = x => console.log(x) || x
 
-export const Right = x => ({
-  chain: f => f(x),
-  map: f => Right(f(x)),
-  fold: (f, g) => g(x),
-})
+const Either = (() => {
+  const Right = x => ({
+    chain: f => f(x),
+    ap: other => other.map(x),
+    alt: other => Right(x),
+    extend: f => f(Right(x)),
+    concat: other =>
+      other.fold(
+        x => other,
+        y => Right(x.concat(y)),
+      ),
+    traverse: (of, f) => f(x).map(Right),
+    map: f => Right(f(x)),
+    fold: (_, g) => g(x),
+    toString: () => `Right(${x})`,
+  })
 
-export const Left = x => ({
-  chain: f => Left(x),
-  map: f => Left(x),
-  fold: (f, g) => f(x),
-})
+  const Left = x => ({
+    chain: _ => Left(x),
+    ap: _ => Left(x),
+    extend: _ => Left(x),
+    alt: other => other,
+    concat: _ => Left(x),
+    traverse: (of, _) => of(Left(x)),
+    map: _ => Left(x),
+    fold: (f, _) => f(x),
+    toString: () => `Left(${x})`,
+  })
 
-export const tryCatch = f => {
-  try {
-    return Right(f())
-  } catch (error) {
-    return Left(error)
+  const of = Right
+  const tryCatch = f => {
+    try {
+      return Right(f())
+    } catch (e) {
+      return Left(e)
+    }
   }
-}
 
-export const fromNullable = x => (x != null ? Right(x) : Left(x))
+  const fromNullable = x => (x != null ? Right(x) : Left(x))
+
+  return {Right, Left, of, tryCatch, fromNullable}
+})()
 
 export const Task = fork => ({
   fork,

3e1ce72ddb0549a717528d41acea4de2a37119a3
Yeasir Ahamed <yahamed007@gmail.com>
2021-11-17 11:15:41 +0600

    remove Task example from Either

--- either/either.js
+++ either/either.js
@@ -1,37 +1,3 @@
-import {Either, Log, Task} from '../utils/utils.js'
-import fs from 'fs'
+import {Either, Log} from '../utils/utils.js'
 
 const {Right, Left, of, tryCatch, fromNullable} = Either
-
-const App_ = () =>
-  fs.readFile('./config.json', 'utf-8', (error, contents) => {
-    if (error) throw error
-    console.log(error, contents)
-    const newContent = contents.replace(/3/g, '6')
-    fs.writeFile('./config1.json', newContent, (err, _) => {
-      if (err) throw err
-      console.log('success')
-    })
-  })
-
-const readFile = (path, enc) =>
-  Task((rej, res) =>
-    fs.readFile(path, enc, (err, contents) => (err ? rej(err) : res(contents))),
-  )
-
-const writeFile = (path, contents) =>
-  Task((rej, res) =>
-    fs.writeFile(path, contents, (err, content) =>
-      err ? rej(err) : res(content),
-    ),
-  )
-
-const App = () =>
-  readFile('./config.json', 'utf-8')
-    .map(contents => contents.replace(/3/g, '6'))
-    .chain(newContent => writeFile('./config1.json', newContent))
-
-App().fork(
-  () => console.log('error'),
-  () => console.log('Success'),
-)
---
+++ task/task.js
@@ -0,0 +1,38 @@
+import path from 'path'
+import fs from 'fs'
+import {Task} from '../utils/utils.js'
+
+const App_ = () =>
+  fs.readFile('./config.json', 'utf-8', (error, contents) => {
+    if (error) throw error
+    console.log(error, contents)
+    const newContent = contents.replace(/3/g, '6')
+    fs.writeFile('./config1.json', newContent, (err, _) => {
+      if (err) throw err
+      console.log('success')
+    })
+  })
+
+const readFile = (filePath, enc) =>
+  Task((rej, res) =>
+    fs.readFile(filePath, enc, (err, contents) =>
+      err ? rej(err) : res(contents),
+    ),
+  )
+
+const writeFile = (filePath, contents) =>
+  Task((rej, res) =>
+    fs.writeFile(filePath, contents, (err, content) =>
+      err ? rej(err) : res(content),
+    ),
+  )
+
+const App = () =>
+  readFile(path.resolve(__dirname, 'config.json'), 'utf-8')
+    .map(contents => contents.replace(/3/g, '6'))
+    .chain(newContent => writeFile('./config1.json', newContent))
+
+App().fork(
+  () => console.log('error'),
+  () => console.log('Success'),
+)

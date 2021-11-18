5ea3502dac9ae8a04101b8ca3e0ff3bf7dbee544
Yeasir Ahamed <yahamed007@gmail.com>
2021-11-19 02:37:13 +0600

    add either example

--- either/either.js
+++ either/either.js
@@ -1,3 +1,15 @@
+import fs from 'fs'
 import {Either, Log} from '../utils/utils.js'
 
 const {Right, Left, of, tryCatch, fromNullable} = Either
+
+const readFileSync = path => tryCatch(() => fs.readFileSync(path))
+const parseJson = content => tryCatch(() => JSON.parse(content))
+
+const getPort = () =>
+  readFileSync('./config.json')
+    .chain(parseJson)
+    .map(config => config.port)
+    .fold((x = 3000) => Log(x), Log)
+
+getPort()

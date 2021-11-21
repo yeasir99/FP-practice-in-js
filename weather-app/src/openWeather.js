42339db135ab6ecf1bc6f74d03689ccc0f3ba996
Yeasir Ahamed <yahamed007@gmail.com>
2021-11-22 00:52:16 +0600

    add openWeather module

---
+++ weather-app/src/openWeather.js
@@ -0,0 +1,19 @@
+import apiKey from './apiKey'
+import {Task, compose} from '../../utils/utils'
+
+const getOpenWeather = zip =>
+  `http://api.openweathermap.org/data/2.5/forecast?zip=${zip},us&appid=${apiKey}`
+
+const fetchIt = url =>
+  Task((rej, res) =>
+    fetch(url)
+      .then(x => x.json())
+      .then(res)
+      .catch(rej),
+  )
+
+const openWeather = {
+  fetch: compose(fetchIt, getOpenWeather),
+}
+
+export default openWeather
